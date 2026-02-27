9cc246fb8fb18a344dab79261509c29322f7b7c3
Igor Miranda <igormiranda@MacBook-Air-de-Igor.local>
2026-02-27 11:10:28 -0300

    Update 2702.4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { createServer } = require('http');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = process.env.HOSTNAME || '0.0.0.0';
+const hostname = '0.0.0.0';
 const port = Number(process.env.PORT || 3000);
 
 const app = next({ dev, hostname, port });
@@ -17,3 +17,4 @@ app.prepare().then(() => {
     console.log(`> Ready on http://${hostname}:${port}`);
   });
 });
+
